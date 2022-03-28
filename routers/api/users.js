8cf866e7494bc86cb619b661b755fa02f37e0773
JorgeCarlotto <jorgemcarlotto@gmail.com>
2022-03-28 15:50:44 -0300

    Express validator check and validation users ok

--- routers/api/users.js
+++ routers/api/users.js
@@ -1,8 +1,18 @@
 const router =require('express').Router();
 const bcrypt = require('bcryptjs');
 const { User } = require('../../db')
+const { check, validationResult } = require('express-validator')
 
-router.post('/register',async (req, res) => {
+router.post('/register',[
+    check('username',"Nombre de usuario Obligatorio").not().isEmpty(),
+    check('password',"Password es Obligatorio").not().isEmpty(),
+    check('email',"El mail debe ser correcto").isEmail(),
+],async (req, res) => {
+
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(422).json({errores:errors.array()})  
+    }
 
     req.body.password = bcrypt.hashSync(req.body.password,10);
     const user = await User.create(req.body);
