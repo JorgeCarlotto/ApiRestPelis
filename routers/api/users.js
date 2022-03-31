ae0e6c34e82a900b927f969b367d737013e8551d
JorgeCarlotto <jorgemcarlotto@gmail.com>
2022-03-31 13:55:20 -0300

    create user token ok - moment and jwt

--- routers/api/users.js
+++ routers/api/users.js
@@ -2,6 +2,8 @@ const router =require('express').Router();
 const bcrypt = require('bcryptjs');
 const { User } = require('../../db')
 const { check, validationResult } = require('express-validator')
+const moment = require('moment')
+const jwt = require('jwt-simple')
 
 router.post('/register',[
     check('username',"Nombre de usuario Obligatorio").not().isEmpty(),
@@ -19,5 +21,30 @@ router.post('/register',[
     res.json(user)
 });
 
+router.post('/login', async (req, res) => {
+  const user = await User.findOne({
+    where: {email : req.body.email}
+  })
+  if (user){
+    const igual = bcrypt.compareSync(req.body.password, user.password)
+      if(igual){
+        res.json({ success: createToken(user)})
+      }else{
+        res.json({ error: 'contraseña incorrecta' })
+      }
+  }else{
+    res.json({ error: 'Usuario y/o contraseña incorrecta' })
+  }
+
+});
+
+const createToken = (user)=>{
+   const payload = {
+     usuarioId : user.id,
+     createAt:moment().unix(),
+     expireAt:moment().add(5,'minutes').unix()
+   }
+   return jwt.encode(payload,'asd scret')
+}
 
 module.exports = router;
