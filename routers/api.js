602ea06c3c19333a9a445a6bcaa6a55a2da2f283
JorgeCarlotto <jorgemcarlotto@gmail.com>
2022-03-31 14:30:10 -0300

    middleware token Ok

--- routers/api.js
+++ routers/api.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 
+const middleware = require('./middleware')
+
 const apiPelisRouter = require('./api/pelis');
 
 const apiUsersRouter = require('./api/users');
 
-router.use('/pelis',apiPelisRouter);
+router.use('/pelis', middleware.checkToken, apiPelisRouter);
 router.use('/users',apiUsersRouter);
 
 module.exports = router;
